40f512b85be9bea863f88141e3305b5c9e777338
kylepunta <kylepurcell1998@gmail.com>
2025-05-02 12:53:37 +0100

    Created a header and main in app component. Created a heading for the game, score and best score in the header element. Applied CSS padding, background color, color and flexbox to the header element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Pokemon Card Memory Game</h1>
+        </div>
+        <div>
+          <div>
+            <h3 id="score-heading">Score: </h3>
+            <p id="score"></p>
+          </div>
+          <div>
+            <h3 id="best-score-heading">Best Score: </h3>
+            <p id="best-score"></p>
+          </div>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
 }
 
 export default App;
